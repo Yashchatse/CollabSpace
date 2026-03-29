cf5d1dd057d66ba3a48a79cd941b66dfd7225eb4
Yash Chatse <116303848+Yashchatse@users.noreply.github.com>
2026-03-29 15:42:04 +0530

    added the config for the docker and docker compose

---
+++ init-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE collabspace_auth;
+CREATE DATABASE collabspace_user;
+CREATE DATABASE collabspace_project;
+CREATE DATABASE collabspace_billing;
+CREATE DATABASE collabspace_notification;
